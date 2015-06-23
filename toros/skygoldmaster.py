7727d152079be791b9c3dba573f5f46299ce58a5
Bruno Sanchez <bruno.sanchez.63@gmail.com>
2015-06-23 13:27:41 -0300

    Added mask option for reference fits file

--- toros/skygoldmaster.py
+++ toros/skygoldmaster.py
@@ -1,15 +1,15 @@
 """@package skygoldmaster
-    
+
     Sky Gold Master module
     ----------------------
-    
+
     A collection of tools to create a reference image for the sky
     for the Transient Optical Robotic Observatory of the South (TOROS).
-    
+
     Martin Beroiz - 2014
-    
+
     email: <martinberoiz@phys.utb.edu>
-    
+
     University of Texas at San Antonio
 """
 
@@ -25,19 +25,19 @@ from reproject import reproject_interp
 
 _REF_IMAGE_NAME = 'master2010.fits'
 
-def getReference(image_in, header_in = None, reference_fits_file = None):
+def getReference(image_in, header_in = None, reference_fits_file = None, ref_maskfile=None):
     """Return the reference image aligned with the input image.
-    
-    getReference accepts a numpy array (masked or not) or a header with WCS information and optionally a master reference fits file and return 
+
+    getReference accepts a numpy array (masked or not) or a header with WCS information and optionally a master reference fits file and return
     a reprojected reference image array for the same portion of the sky.
     Return reference_image"""
-        
+
     #It's assumed that the default master has WCS info
     if reference_fits_file is None:
         refHasWCS = True
     else:
         refHasWCS = _checkIfWCS(fits.getheader(reference_fits_file))
-        
+
     #Check if there is a header available
     if (header_in is not None) and _checkIfWCS(header_in) and refHasWCS:
         #reproject with reproject here...
@@ -46,41 +46,45 @@ def getReference(image_in, header_in = None, reference_fits_file = None):
             reference_fits_file = pkg_resources.resource_filename('toros.resources', _REF_IMAGE_NAME)
 
         refhdu = fits.open(reference_fits_file)[0]
-        ref_mask = refhdu.data < 0
-        refhdu_mask = fits.PrimaryHDU(ref_mask.astype('float'), header=refhdu.header)
+        if ref_maskfile is None:
+            ref_mask = refhdu.data < 0
+            refhdu_mask = fits.PrimaryHDU(ref_mask.astype('float'), header=refhdu.header)
+        else:
+            refhdu_mask = fits.open(ref_maskfile)[0]
+            ref_mask = refhdu_mask.data
         ref_reproj_data, __ = reproject_interp(refhdu, header_in)
         ref_reproj_mask, __ = reproject_interp(refhdu_mask, header_in)
         gold_master = np.ma.array(data=ref_reproj_data, mask=ref_reproj_mask)
     else:
         gold_master = _no_wcs_available(image_in, reference_fits_file)
-    
+
     return gold_master
-    
-    
+
+
 def _checkIfWCS(header):
     my_wcs = wcs.WCS(header)
     return True if my_wcs.wcs.ctype[0] else False
 
 
 def _no_wcs_available(image_in, reference_fits_file):
-    
+
     if not isinstance(image_in, np.ma.MaskedArray):
         image_in_ma = np.ma.array(image_in)
     else:
         image_in_ma = image_in
     test_srcs = findSources(image_in_ma)[:50]
-    
+
     if reference_fits_file is None:
-        ref_image = fits.getdata(pkg_resources.resource_filename('toros.resources', _REF_IMAGE_NAME))           
+        ref_image = fits.getdata(pkg_resources.resource_filename('toros.resources', _REF_IMAGE_NAME))
         ref_mask  = ref_image < 0
         ref_image = np.ma.array(ref_image, mask=ref_mask)
         ref_sources = None
     else:
-        ref_image = fits.getdata(reference_fits_file)           
+        ref_image = fits.getdata(reference_fits_file)
         ref_mask  = ref_image < 0
         ref_image = np.ma.array(ref_image, mask=ref_mask)
         ref_sources = findSources(ref_image)[:70]
-    
+
     M = registration.findAffineTransform(test_srcs, ref_srcs = ref_sources)
 
     #SciPy Affine transformation transform a (row,col) pixel according to pT+s where p is in the _output_ image,
@@ -100,20 +104,20 @@ def _no_wcs_available(image_in, reference_fits_file):
     P = np.array([[0,1],[1,0]])
     Mrcinv_rot = P.dot(Minv[:2,:2]).dot(P)
     Mrcinv_offset = P.dot(Minv[:2,2])
-    
+
     #M_rot = M[:2,:2]
     #M_offset = M[:2,2]
     #M_rot_inv = np.linalg.inv(M_rot)
     #Mrcinv_rot = P.dot(M_rot_inv).dot(P)
     #Mrcinv_offset = -P.dot(M_rot_inv).dot(M_offset)
-    
+
     gold_master = ndimage.interpolation.affine_transform(ref_image, Mrcinv_rot, offset=Mrcinv_offset, output_shape=image_in.shape)
     #gold_master_mask = ndimage.interpolation.affine_transform(ref_img.mask, M_rot, offset=M_offset, output_shape=image_in.shape)
     gold_master = np.ma.array(gold_master, mask=gold_master < 0)
-        
+
     return gold_master
-    
-    
+
+
 def makeSourcesMask(dataImg, noiseLvl = 3.):
 
     m, s = bkgNoiseSigma(dataImg, noiseLvl = noiseLvl)
@@ -123,11 +127,11 @@ def makeSourcesMask(dataImg, noiseLvl = 3.):
         srcsMask.set_fill_value(False)
         srcsMask = srcsMask.filled()
     return srcsMask
-    
-    
+
+
 def bkgNoiseSigma(dataImg, noiseLvl = 3.0):
     """Return background mean and std. dev. of sky background.
-    
+
     Calculate the background (sky) mean value and a measure of its standard deviation.
     Background is considered anything below 'noiseLvl' sigmas.
     goodPixMask is a mask containing the good pixels that should be considered.
@@ -148,8 +152,8 @@ def bkgNoiseSigma(dataImg, noiseLvl = 3.0):
         m, s = dataImg[bkgMask].mean(), dataImg[bkgMask].std()
 
     return m, s
-    
-    
+
+
 def findSources(image):
     """Return sources sorted by brightness.
     """
@@ -178,11 +182,11 @@ def findSources(image):
     #Eliminate here all 1 pixel sources
     all_objects = [[ind + 1, aslice] for ind, aslice in enumerate(ndimage.find_objects(srcs_labels))
                                                 if srcs_labels[aslice].shape != (1,1)]
-    lum = np.array([obj_params_with_offset(img1, srcs_labels, aslice, lab_idx) 
+    lum = np.array([obj_params_with_offset(img1, srcs_labels, aslice, lab_idx)
                 for lab_idx, aslice in all_objects])
 
     lum = lum[lum[:,0].argsort()[::-1]]  #sort by brightness highest to smallest
 
     return lum[:,1:]
 
-    
+
--- toros/subtraction.py
+++ toros/subtraction.py
@@ -1,28 +1,28 @@
 """@package subtraction
-    
+
     Subtraction module
     -----------------
-    
+
     A collection of tools to perform optimal image differencing
     for the Transient Optical Robotic Observatory of the South (TOROS).
-    
+
     ### Usage example (from python):
-    
+
         >>> from toros import subtraction
         >>> conv_image, optimalKernel, background = ois.getOptimalKernelAndBkg(image, referenceImage)
-    
+
     (conv_image is the least square optimal approximation to image)
-    
+
     See getOptimalKernelAndBkg docstring for more options.
-    
+
     ### Command line arguments:
     * -h, --help: Prints this help and exits.
     * -v, --version: Prints version information and exits.
-        
+
     Martin Beroiz - 2014
-    
+
     email: <martinberoiz@phys.utb.edu>
-    
+
     University of Texas at San Antonio
 """
 
@@ -39,32 +39,34 @@ def __gauss(shape = (10,10), center=None, sx=2, sy=2):
     norm = math.sqrt(2*math.pi*(sx**2)*(sy**2))
     return np.exp(-0.5*((x-x0)**2/sx**2 + (y-y0)**2/sy**2))/norm
 
-def __CVectorsForGaussBasis(refImage, kernelShape, gaussList, modBkgDeg, badPixMask):
+def __CVectorsForGaussBasis(refImage, kernelShape, gaussList, \
+                            modBkgDeg, badPixMask):
     #degMod is the degree of the modulating polyomial
     kh, kw = kernelShape
-    
+
     v,u = np.mgrid[:kh,:kw]
     C = []
     for aGauss in gaussList:
         if 'modPolyDeg' in aGauss: degMod = aGauss['modPolyDeg']
         else: degMod = 2
-        
+
         allUs = [pow(u,i) for i in range(degMod + 1)]
         allVs = [pow(v,i) for i in range(degMod + 1)]
-        
+
         if 'center' in aGauss: center = aGauss['center']
         else: center = None
-        gaussK = __gauss(shape=(kh,kw), center=center, sx=aGauss['sx'], sy=aGauss['sy'])
-        
+        gaussK = __gauss(shape=(kh,kw), center=center, \
+                         sx=aGauss['sx'], sy=aGauss['sy'])
+
         if badPixMask is not None:
-            newC = [np.ma.array(signal.convolve2d(refImage, gaussK * aU * aV, mode='same'), \
-                                mask = badPixMask) \
+            newC = [np.ma.array(signal.convolve2d(refImage, gaussK * aU * aV, \
+                                             mode='same'), mask = badPixMask) \
                     for i, aU in enumerate(allUs) \
                     for aV in allVs[:degMod+1-i]]
         else:
             newC = [signal.convolve2d(refImage, gaussK * aU * aV, mode='same') \
                     for i, aU in enumerate(allUs) for aV in allVs[:degMod+1-i]]
-        
+
         C.extend(newC)
     return C
 
@@ -91,14 +93,16 @@ def __CVectorsForDeltaBasis(refImage, kernelShape, badPixMask):
 
     return C
 
-def __getCVectors(refImage, kernelShape, gaussList, modBkgDeg = 2, badPixMask = None):
+def __getCVectors(refImage, kernelShape, gaussList, modBkgDeg = 2, \
+                  badPixMask = None):
 
     C = []
     if gaussList is not None:
-        C.extend(__CVectorsForGaussBasis(refImage, kernelShape, gaussList, modBkgDeg, badPixMask))
+        C.extend(__CVectorsForGaussBasis(refImage, kernelShape, gaussList, \
+                                         modBkgDeg, badPixMask))
     else:
         C.extend(__CVectorsForDeltaBasis(refImage, kernelShape, badPixMask))
-    
+
     #finally add here the background variation coefficients:
     h, w = refImage.shape
     y, x = np.mgrid[:h,:w]
@@ -109,7 +113,8 @@ def __getCVectors(refImage, kernelShape, gaussList, modBkgDeg = 2, badPixMask = 
         newC = [np.ma.array(anX * aY, mask = badPixMask) \
                 for i, anX in enumerate(allXs) for aY in allYs[:modBkgDeg+1-i]]
     else:
-        newC = [anX * aY for i, anX in enumerate(allXs) for aY in allYs[:modBkgDeg+1-i]]
+        newC = [anX * aY for i, anX in enumerate(allXs) \
+                for aY in allYs[:modBkgDeg+1-i]]
 
     C.extend(newC)
     return C
@@ -124,14 +129,15 @@ def __coeffsToKernel(coeffs, gaussList, kernelShape = (10,10)):
         for aGauss in gaussList:
             if 'modPolyDeg' in aGauss: degMod = aGauss['modPolyDeg']
             else: degMod = 2
-        
+
             allUs = [pow(u,i) for i in range(degMod + 1)]
             allVs = [pow(v,i) for i in range(degMod + 1)]
-        
+
             if 'center' in aGauss: center = aGauss['center']
             else: center = None
-            gaussK = __gauss(shape=kernelShape, center=center, sx=aGauss['sx'], sy=aGauss['sy'])
-        
+            gaussK = __gauss(shape=kernelShape, center=center, sx=aGauss['sx'],\
+                             sy=aGauss['sy'])
+
             ind = 0
             for i, aU in enumerate(allUs):
                 for aV in allVs[:degMod+1-i]:
@@ -158,33 +164,36 @@ def __coeffsToBackground(shape, coeffs, bkgDeg = None):
 
     return mybkg
 
-def getOptimalKernelAndBkg(image, refImage, gaussList = None, bkgDegree = 3, kernelShape = (11,11)):
+def getOptimalKernelAndBkg(image, refImage, gaussList = None,\
+                           bkgDegree = 3, kernelShape = (11,11)):
     """Do Optimal Image Subtraction and return optimal kernel and background.
-        
-    This is an implementation of the Optimal Image Subtraction algorith of Alard&Lupton.
-    It returns the best kernel and background fit that match the two images.
-    
-    gaussList is a list of dictionaries containing data of the gaussians used in the decomposition
-    of the kernel. Dictionary keywords are:
+
+    This is an implementation of the Optimal Image Subtraction algorith of
+    Alard&Lupton. It returns the best kernel and background fit that match the
+    two images.
+
+    gaussList is a list of dictionaries containing data of the gaussians
+    used in the decomposition of the kernel. Dictionary keywords are:
     center, sx, sy, modPolyDeg
-    If gaussList is None (default value), the OIS will try to optimize the value of each pixel in
-    the kernel.
-    
+    If gaussList is None (default value), the OIS will try to optimize
+    the value of each pixel in the kernel.
+
     bkgDegree is the degree of the polynomial to fit the background.
-    
+
     kernelShape is the shape of the kernel to use.
-    
+
     Return (optimal_image, kernel, background)
     """
-    
+
     kh, kw = kernelShape
     if kw % 2 != 1 or kh % 2 != 1:
         print("This can only work with kernels of odd sizes.")
         return None, None, None
-    
+
     badPixMask = None
     if isinstance(refImage, np.ma.MaskedArray):
-        refMask = ndimage.binary_dilation(refImage.mask.astype('uint8'), structure=np.ones(kernelShape))
+        refMask = ndimage.binary_dilation(refImage.mask.astype('uint8'), \
+                                          structure=np.ones(kernelShape))
         badPixMask = refMask.astype('bool')
         if isinstance(image, np.ma.MaskedArray):
              badPixMask += image.mask
@@ -195,8 +204,9 @@ def getOptimalKernelAndBkg(image, refImage, gaussList = None, bkgDegree = 3, ker
     m = np.array([[(ci*cj).sum() for ci in C] for cj in C])
     b = np.array([(image*ci).sum() for ci in C])
     coeffs = np.linalg.solve(m,b)
-    
-    #nKCoeffs is the number of coefficients related to the kernel fit, not the background fit
+
+    #nKCoeffs is the number of coefficients related to the kernel fit,
+    # not the background fit
     if gaussList is None:
         nKCoeffs = kh*kw
     else:
@@ -205,36 +215,43 @@ def getOptimalKernelAndBkg(image, refImage, gaussList = None, bkgDegree = 3, ker
             if 'modPolyDeg' in aGauss: degMod = aGauss['modPolyDeg']
             else: degMod = 2
             nKCoeffs += (degMod + 1)*(degMod + 2)//2
-    
+
     kernel = __coeffsToKernel(coeffs[:nKCoeffs], gaussList, kernelShape)
     background = __coeffsToBackground(image.shape, coeffs[nKCoeffs:])
-    if isinstance(refImage, np.ma.MaskedArray) or isinstance(image, np.ma.MaskedArray):
+    if isinstance(refImage, np.ma.MaskedArray) or \
+            isinstance(image, np.ma.MaskedArray):
         background = np.ma.array(background, mask=badPixMask)
     optimal_image = signal.convolve2d(refImage, kernel, mode='same') + background
 
     return optimal_image, kernel, background
 
 
-def optimalSubtractOnGrid(image, refImage, gaussList = None, bkgDegree = 3, kernelShape = (11,11), gridShape=(2,2)):
-    """Implement Optimal Image Subtraction on a grid and return the optimal subtraction
-        
-    This is an implementation of the Optimal Image Subtraction algorith of Alard&Lupton(1998) and Bramich(2010)
+def optimalSubtractOnGrid(image, refImage, gaussList = None, bkgDegree = 3,\
+                          kernelShape = (11,11), gridShape=(2,2)):
+    """Implement Optimal Image Subtraction on a grid and return the optimal
+    subtraction
+
+    This is an implementation of the Optimal Image Subtraction algorith of
+    Alard&Lupton(1998) and Bramich(2010)
     It returns the optimal subtraction between image and refImage.
-    
-    gaussList is a list of dictionaries containing data of the gaussians used in the multigaussian decomposition
-    of the kernel [Alard&Lupton, 1998]. Dictionary keywords are:
+
+    gaussList is a list of dictionaries containing data of the gaussians
+    used in the multigaussian decomposition of the kernel [Alard&Lupton, 1998].
+    Dictionary keywords are:
     center, sx, sy, modPolyDeg
-    If gaussList is None (default value), the OIS will try to optimize the value of each pixel in
-    the kernel [Bramich, 2010].
-    
+
+    If gaussList is None (default value), the OIS will try to optimize the value
+    of each pixel in the kernel [Bramich, 2010].
+
     bkgDegree is the degree of the polynomial to fit the background.
-    
+
     kernelShape is the shape of the kernel to use.
 
-    gridShape is a tuple containing the number of vertical and horizontal divisions of the grid.
+    gridShape is a tuple containing the number of vertical and horizontal
+    divisions of the grid.
 
     This method does not interpolate between the grids.
-    
+
     Return (subtraction_array)
     """
     ny, nx = gridShape
@@ -244,14 +261,18 @@ def optimalSubtractOnGrid(image, refImage, gaussList = None, bkgDegree = 3, kern
     #normal slices with no border
     stamps_y = [slice(h*i/ny, h*(i+1)/ny, None)  for i in range(ny)]
     stamps_x = [slice(w*i/nx, w*(i+1)/nx, None)  for i in range(nx)]
-    
+
     #slices with borders where possible
-    slc_wborder_y = [slice(max(0, h*i/ny - (kh-1)/2), min(h, h*(i+1)/ny + (kh-1)/2), None)  for i in range(ny)]
-    slc_wborder_x = [slice(max(0, w*i/nx - (kw-1)/2), min(w, w*(i+1)/nx + (kw-1)/2), None)  for i in range(nx)]
-    
-    imgStamps = [image[sly, slx] for sly in slc_wborder_y for slx in slc_wborder_x]
-    refStamps = [refImage[sly, slx] for sly in slc_wborder_y for slx in slc_wborder_x]
-        
+    slc_wborder_y = [slice(max(0, h*i/ny - (kh-1)/2), \
+                           min(h, h*(i+1)/ny + (kh-1)/2), None)  for i in range(ny)]
+    slc_wborder_x = [slice(max(0, w*i/nx - (kw-1)/2), \
+                           min(w, w*(i+1)/nx + (kw-1)/2), None)  for i in range(nx)]
+
+    imgStamps = [image[sly, slx] for sly in slc_wborder_y \
+                 for slx in slc_wborder_x]
+    refStamps = [refImage[sly, slx] for sly in slc_wborder_y \
+                 for slx in slc_wborder_x]
+
     #After we do the subtraction we need to crop the extra borders in the stamps.
     #The recover_slices are the prescription for what to crop on each stamp.
     recover_slices = []
@@ -266,13 +287,14 @@ def optimalSubtractOnGrid(image, refImage, gaussList = None, bkgDegree = 3, kern
             if slx_stop == 0: slx_stop = None
             slx = slice(w*j/nx - start_border_x, slx_stop, None)
             recover_slices.append([sly,slx])
-            
+
     #Here do the subtraction on each stamp
-    subtractCollage = np.ma.empty(image.shape)    
+    subtractCollage = np.ma.empty(image.shape)
     stamp_slices = [[sly, slx] for sly in stamps_y for slx in stamps_x]
-    for ind, ((sly_out, slx_out), (sly_in, slx_in)) in enumerate(zip(recover_slices, stamp_slices)):
+    for ind, ((sly_out, slx_out), (sly_in, slx_in)) in \
+            enumerate(zip(recover_slices, stamp_slices)):
         opti, ki, bgi = getOptimalKernelAndBkg(imgStamps[ind], refStamps[ind], \
-                                                                 gaussList, bkgDegree, kernelShape)
+                                              gaussList, bkgDegree, kernelShape)
         subtractCollage[sly_in, slx_in] = (imgStamps[ind] - opti)[sly_out, slx_out]
-        
+
     return subtractCollage
