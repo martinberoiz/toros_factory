25e2d26dd6e0cf0fadba684d2c0d8dca8f3a5f8b
Martin Beroiz <martinberoiz@yahoo.com>
2015-07-13 12:09:00 -0500

    Deleted cstar_ref_image.fits and added install_requires in setup.py. This fixes issue #19

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(name='toros',
       author_email='martinberoiz@phys.utb.edu',
       url='http://toros.phys.utb.edu',
       packages=['toros','toros.instrument', 'toros.resources'],
-      package_data={'': ['master2010.fits', '*.npy', 'cstar_mags_catalog.txt']}
+      package_data={'': ['master2010.fits', '*.npy', 'cstar_mags_catalog.txt']},
+      install_requires=["numpy","scipy","astropy"],
      )
