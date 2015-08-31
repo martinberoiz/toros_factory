88bc144a0056cf5f9ffd0d0115272a9e3407480a
Martin Beroiz <martinberoiz@yahoo.com>
2015-08-31 10:05:52 -0500

    Added trackremoval code from Americo.

--- toros/trackremoval.py
+++ toros/trackremoval.py
@@ -1,14 +1,44 @@
 """@package trackremoval
-    
-    Track Removal module
-    -------------------------
-    
-    A collection of tools to remove satellites and aircraft tracks
-    for the Transient Optical Robotic Observatory of the South (TOROS).
-    
-    Martin Beroiz - 2014
-    
-    email: <martinberoiz@phys.utb.edu>
-    
-    University of Texas at San Antonio
+
+    Satellite track removal module
+    ------------------------------
+
+    Function to find satellite tracks in astro images.
+    The function needs to be feed with a NP array from a FITS file
+    The result will be an array with zeros and ones, in which the ones are the borders of the satellite tracks
+
+    Americo Hinojosa - 2015
+
+    email: <americo.hinojosalee01@utrgv.edu>
+
+    University of Texas at Rio Grande Valley
 """
+
+import numpy as np
+from skimage.filter import canny
+from skimage.transform import (hough_line, hough_line_peaks, probabilistic_hough_line)
+from astropy.stats import sigma_clip
+from skimage import draw
+
+def removeLines(image):
+
+    #Sigma Clipping
+    clipped = sigma_clip(image, 5, 1)
+
+    img = clipped
+    satracks = np.zeros_like(img)
+    median = np.median(img)
+
+    #Edge finding function
+    edges = canny(img, sigma=3, low_threshold=median*0.1666, high_threshold=median*.25)
+
+    #Identify current lines
+    lines = probabilistic_hough_line(edges, threshold=30, line_length=20, line_gap=3)
+    for line in lines:
+        x, y = line
+        x0, y0 = x[0], y[0]
+        x1, y1 = x[1], y[1]
+        rr, cc = draw.line(y0, y1, x0, x1)
+        satracks[cc, rr] = 1
+
+    return satracks
